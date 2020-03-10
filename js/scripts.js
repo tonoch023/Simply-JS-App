2dbc5af5e88c17aee9ac2296412613a8869451a1
tonoch023 <51219998+tonoch023@users.noreply.github.com>
2020-03-10 11:48:43 -0700

    Clean merge Conflicts

--- js/scripts.js
+++ js/scripts.js
@@ -21,19 +21,10 @@ var pokedexRepository = [
  }
 ];
 
-<<<<<<< HEAD
-
 for(i=0;i<4;i++){
   if(pokedexRepository[i].height > 1){
     document.write(pokedexRepository[i].name,',',' Wow thats a big one ', '<br>');
   } else if (pokedexRepository[i].height < 1) {
     document.write(pokedexRepository[i].name, ' ', '<br>')
   }
-=======
-for (i = 0; i < 4; i++) {
- document.write(pokedexRepository[i].lenght)
-}
-if (pokedexRepository.height < 1){
-  document.write('wow thats a big one')
->>>>>>> e294ad6dd92839a947c1aa45b50449f61fa1a390
-}
+
