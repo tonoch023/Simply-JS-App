c00e0fb2a7f06cd52971c501e63a6c54e6f1e6c0
tonoch023 <tonoch023@gmail.com>
2020-03-11 22:13:53 -0700

    Replaced for loop, with foreach loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,90 @@
-var pokedexRepository = [
-  {
-    name: 'Bulbasaur',
-    height: .7,
-    type: ['Grass','Poison']
- },
- {
-   name: 'Arbok',
-   height: 3.5,
-   type: ['Poison']
- },
- {
-   name: 'Nidoking',
-   height: 1.4,
-   type: ['Ground','Poison']
- },
- {
-   name: 'Scyther',
-   height: 1.5,
-   type: ['Bug','Flying']
- }
-];
 
-<<<<<<< HEAD
+  var pokedexRepository = [
+    {
+      name: 'Bulbasaur',
+      height: .7,
+      type: ['Grass','Poison']
+   },
+   {
+     name: 'Arbok',
+     height: 3.5,
+     type: ['Poison']
+   },
+   {
+     name: 'Nidoking',
+     height: 1.4,
+     type: ['Ground','Poison']
+   },
+   {
+     name: 'Scyther',
+     height: 1.5,
+     type: ['Bug','Flying']
+   }
+  ];
 
-for(i=0;i<4;i++){
-  if(pokedexRepository[i].height > 1){
-    document.write(pokedexRepository[i].name,',',' Wow thats a big one ', '<br>');
-  } else if (pokedexRepository[i].height < 1) {
-    document.write(pokedexRepository[i].name, ' ', '<br>')
+
+    var pokedexRepository2 = [
+      {
+        name: 'Charizard',
+        height: 2.5,
+        type: ['Fire','Flying']
+     },
+     {
+       name: 'Pikachu',
+       height: .5,
+       type: ['Electric']
+     }
+   ];
+
+
+  // function printedArraysDetails(pokemonList){
+  //   for(i=0;i<pokemonList.length  ;i++){
+  //     if(pokedexRepository[i].height > 1){
+  //       document.write(pokemonList[i].name,',',' Wow thats a big one ', '<br>');
+  //     } else if (pokedexRepository[i].height < 1) {
+  //       document.write(pokemonList[i].name, ' ', '<br>');
+  //     }
+  //   }
+  // }
+
+  pokedexRepository.forEach(function(currentName){
+    if (currentName.height > 1){
+      document.write(currentName.name, ' Wow, thats a big one!', '<br>');
+    }else if(currentName.height< 1){
+      document.write(currentName.name, '<br>');
+    }});
+
+
+
+  var names = ['John', 'Anne', 'Carly'];
+
+  function loopBlockFunction(currentName){
+    console.log(currentName);
   }
-=======
-for (i = 0; i < 4; i++) {
- document.write(pokedexRepository[i].lenght)
+  names.forEach(loopBlockFunction);
+
+  var anne = {
+  name: 'Anne',
+  age: 38,
+  children: []
+};
+
+Object.keys(anne).forEach(function(property) {
+  console.log(anne[property]);
+});
+
+  printedArraysDetails(pokedexRepository2);                                                                  document.write('<br>');  //line break
+  printedArraysDetails(pokedexRepository);
+
+
+function div(dividend, divisor){
+  if ( divisor === 0){
+    return "Youre trying to divided by zero"
+  }else{
+    var result= dividend / divisor;
+    return result
+  }
 }
-if (pokedexRepository.height < 1){
-  document.write('wow thats a big one')
->>>>>>> e294ad6dd92839a947c1aa45b50449f61fa1a390
-}
+
+console.log(div(10,0));
+console.log(div(10,5));
