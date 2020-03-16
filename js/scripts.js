dafcef57beabbbc68a040aeb43493844684c6bb1
tonoch023 <tonoch023@gmail.com>
2020-03-15 23:50:48 -0700

    Applying IIFE to repository.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,40 @@
-  var pokedexRepository = [
-    {
-      name: 'Bulbasaur',
-      height: .7,
-      type: ['Grass','Poison']
-   },
-   {
-     name: 'Arbok',
-     height: 3.5,
-     type: ['Poison']
-   },
-   {
-     name: 'Nidoking',
-     height: 1.4,
-     type: ['Ground','Poison']
-   },
-   {
-     name: 'Scyther',
-     height: 1.5,
-     type: ['Bug','Flying']
-   }
-  ];
+var pokedexRepository = (function () {
+  var repository = [{
+    name: 'Bulbasaur',
+    height: .7,
+    type: ['Grass','Poison']
+ },
+ {
+   name: 'Arbok',
+   height: 3.5,
+   type: ['Poison']
+ },
+ {
+   name: 'Nidoking',
+   height: 1.4,
+   type: ['Ground','Poison']
+ },
+ {
+   name: 'Scyther',
+   height: 1.5,
+   type: ['Bug','Flying']
+ }];
+
+  function add(pokemon) {
+    repository.push(pokemon);
+  }
+
+  function getAll() {
+    return repository;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+
 
   // function printedArraysDetails(pokemonList){
   //   for(i=0;i<pokemonList.length  ;i++){
@@ -31,44 +46,9 @@
   //   }
   // }
 
-  pokedexRepository.forEach(function(currentName){
+  pokedexRepository.getAll.forEach(function(currentName){
     if (currentName.height > 1){
       document.write(currentName.name, ' Wow, thats a big one!', '<br>');
     }else if(currentName.height< 1){
       document.write(currentName.name, '<br>');
     }});
-
-
-
-  var names = ['John', 'Anne', 'Carly'];
-
-  function loopBlockFunction(currentName){
-    console.log(currentName);
-  }
-  names.forEach(loopBlockFunction);
-
-  var anne = {
-  name: 'Anne',
-  age: 38,
-  children: []
-};
-
-Object.keys(anne).forEach(function(property) {
-  console.log(anne[property]);
-});
-
-  printedArraysDetails(pokedexRepository2);                                                                  document.write('<br>');  //line break
-  printedArraysDetails(pokedexRepository);
-
-
-function div(dividend, divisor){
-  if ( divisor === 0){
-    return "Youre trying to divided by zero"
-  }else{
-    var result= dividend / divisor;
-    return result
-  }
-}
-
-console.log(div(10,0));
-console.log(div(10,5));
