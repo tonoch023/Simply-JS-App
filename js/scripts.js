48995ed5954f55d21fa733bf4bf52c042cccec1f
tonoch023 <tonoch023@gmail.com>
2020-06-24 16:06:52 -0600

    1.7: APIs, Ajax & Asynchronous Behavior

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
   //Function to add pokemon to repository
   function add(pokemon) {
     //Must be an 'object' type
@@ -28,9 +29,44 @@ let pokemonRepository = (function () {
   }
 
   function showDetails(pokemon) {
-    console.log(
-      `Name: ${pokemon.name}  Height:  ${pokemon.height}  Ability:${pokemon.abilities}  Type: ${pokemon.types}`
-    );
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
+  }
+
+  function loadList() {
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon);
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        // Now we add the details to the item
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
   }
 
   return {
@@ -38,42 +74,14 @@ let pokemonRepository = (function () {
     getAll: getAll,
     addListItem: addListItem,
     showDetails: showDetails,
+    loadList: loadList,
+    loadDetails: loadDetails,
   };
 })();
-// cSpell: disable
-const ivysaur = {
-  name: 'Ivysaur',
-  height: 1,
-  abilities: ['Chlorophyll'],
-  types: ['Grass'],
-};
 
-const charizard = {
-  name: 'Charizard',
-  height: 1.7,
-  abilities: ['Blaze'],
-  types: ['Fire'],
-};
-
-const Charmander = {
-  name: 'Charmander',
-  height: 2.0,
-  abilities: ['Blaze', 'Solar-power'],
-  types: ['Fire'],
-};
-
-const Squirtle = {
-  name: 'Squirtle',
-  height: 0.5,
-  abilities: ['Rain-dish', 'Torrent'],
-  types: ['Monster', 'Water  1'],
-};
-
-pokemonRepository.add(ivysaur);
-pokemonRepository.add(charizard);
-pokemonRepository.add(Charmander);
-pokemonRepository.add(Squirtle);
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
